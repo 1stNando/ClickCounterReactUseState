212732b100f710b6117569549f04de3b82d89781
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-05 18:20:47 -0400

    Step - 2 Setting up state

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function Counter() {
+  // This will introduce useState
+  const valueAndSetMethod /* <- array */ = useState(
+    0 /* initial state value desired */
+  )
+
+  const counter = valueAndSetMethod[0]
+  const setCounter = valueAndSetMethod[1]
+
   return (
     <div>
-      <p>The count is 0</p>
+      <p>The count is {counter}</p>
       <button>Increment</button>
     </div>
   )
